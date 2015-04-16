5cf961d28619c71cc94e048032841d91c0fa34e4
Kevin Carter <kevin.carter@rackspace.com>
2015-04-16 09:35:59 -0500

    Updated license
    This library was licensed GPLv3+ and as the sole copyright owner I
    am choosing to relicense this library Apache version "2.0".

--- cloudlib/__init__.py
+++ cloudlib/__init__.py
@@ -1,19 +1,24 @@
-# =============================================================================
-# Copyright [2014] [Kevin Carter]
-# License Information :
-# This software has no warranty, it is provided 'as is'. It is your
-# responsibility to validate the behavior of the routines and its accuracy
-# using the code provided. Consult the GNU General Public license for further
-# details (see GNU General Public License).
-# http://www.gnu.org/licenses/gpl.html
-# =============================================================================
+# Copyright 2015, Kevin Carter.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 __author__ = 'Kevin Carter'
 __contact__ = 'Kevin Carter'
 __email__ = 'kevin@cloudnull.com'
 __copyright__ = '2014 All Rights Reserved'
-__license__ = 'GPLv3+'
+__license__ = 'Apache2'
 __date__ = '2014-04-20'
-__version__ = '0.3.0'
+__version__ = '0.4.0'
 __status__ = 'Production'
 __appname__ = 'cloudlib'
 __description__ = 'general purpose library for in application use'
